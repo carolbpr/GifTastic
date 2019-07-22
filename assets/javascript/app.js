7fed055829cbd6daa6e3adf73aa9bf07ce87f582
Caro Berry <carolbpr@gmail.com>
2019-07-21 21:37:46 -0500

    Still working on the layout. :(

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,60 +4,64 @@ window.onload = function () {
     $(document).on("click", ".button", displayGifs);
     $("#newGifbutton").on("click", addNewGifbutton);
     $(document).on("click", 'img', imageClick);
-    
+
 }
-defaultLoad=false;
+defaultLoad = false;
 var topics = ["Yes", "No", "Maybe", "Great Job", "Oh no"];
 function addNewbutton() {
     $("#new-button").empty();
-    for (var i= 0; i<topics.length;i++) {
+    for (var i = 0; i < topics.length; i++) {
         var newButton = $('<div>');
         newButton.addClass("button");
-        newButton.attr("data-name",topics[i]);
+        newButton.attr("data-name", topics[i]);
         newButton.text(topics[i]);
         newButton.appendTo("#new-button");
     }
 };
-function displayGifs(value){
+function displayGifs(value) {
     value = $(this).attr("data-name");
     event.preventDefault();
-    $("#gif-pic").empty();
+    $(".gif-pic").empty();
     var apikey = "RwUKJ3DzRRWT64mYuwCnB9SaSYcsbAfo";
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
-        $.ajax({
-            url: queryURL + value + "&api_key=" + apikey,
-            method: "GET"
-        }).then(function (gifsSearch) {
-            var gifApiinfo = gifsSearch.data.slice(0,10);
-            gifApiinfo.forEach(element => {          
+    $.ajax({
+        url: queryURL + value + "&api_key=" + apikey,
+        method: "GET"
+    }).then(function (gifsSearch) {
+        var gifApiinfo = gifsSearch.data.slice(0, 10);
+        gifApiinfo.forEach(element => {
             var newgif = ($('<img>').attr({
                 src: element.images['480w_still'].url
             })
-            .data({
-                still: element.images['480w_still'].url,
-                moving: element.images.original.url,
-                imgClicked: false
-            }));
-            var gifbox = $("<div/>").attr({class:".gif-box", class:"box" });
-            $("#gif-pic").append(gifbox);
-            gifbox.append(newgif);  
-            var gifinfo = $("<div/>").attr({class:".gif-info"});
+                .data({
+                    still: element.images['480w_still'].url,
+                    moving: element.images.original.url,
+                    imgClicked: false
+                }));
+            var gifbox = $("<div/>").attr({ class: "gif-box" });
+            $(".gif-pic").append(gifbox);
+            gifbox.append(newgif);
+
+            var gifinfo = $("<div/>").attr({ class: "gif-info" });
             gifbox.append(gifinfo);
-            var rating = $("<p>").html("<b>Rating:</b> "+ element.rating);
-           gifinfo.append(rating); 
+            var rating = $("<p>").html("<b>Rating:</b> " + element.rating);
+            gifinfo.append(rating);
 
         });
-        });    
+    });
 
 }
-function addNewGifbutton(){
+
+function addNewGifbutton() {
     event.preventDefault();
     var gifInput = $("#gif-input").val().trim();
-    topics.push(gifInput);
-    addNewbutton();
+    if (gifInput !== "") {
+        topics.push(gifInput);
+        addNewbutton();
+    }
+};
 
-  };
-   function imageClick() {
+function imageClick() {
     if ($(this).data("imgClicked")) {
         $(this).data("imgClicked", false);
         $(this).attr("src", $(this).data("still"));
